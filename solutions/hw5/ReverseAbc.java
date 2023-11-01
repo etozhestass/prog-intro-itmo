9590281b5b9823b3efb11a215024e8cc8835557f
etozhestass <stas.kalachev04@gmail.com>
2023-11-02 01:46:11 +0300

    hw5 ReverseAbc of idea problem with array of Strings(Objects)

--- solutions/hw5/ReverseAbc.java
+++ solutions/hw5/ReverseAbc.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class ReverseAbc {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
-            String[][] matrix = new String[1][1];
+            long[][] matrix = new long[1][1];
             int[] ends = new int[1];
             int column = 0;
             while (scan.hasNextLine()) {
@@ -17,7 +17,7 @@ public class ReverseAbc {
                     matrix = Arrays.copyOf(matrix, column * 2);
                     ends = Arrays.copyOf(ends, column * 2);
                     for (int fill = column; fill < matrix.length; fill++) {
-                        matrix[fill] = new String[1];
+                        matrix[fill] = new long[1];
                     }
                 }
 
@@ -26,7 +26,7 @@ public class ReverseAbc {
                     if (count >= lenRow) {
                         matrix[column] = Arrays.copyOf(matrix[column], lenRow * 2);
                     }
-                    matrix[column][count] = scanString.nextAbc();
+                    matrix[column][count] = parseAbc(scanString.nextAbc());
                     count++;
                 }
                 ends[column] = count;
@@ -35,12 +35,47 @@ public class ReverseAbc {
             }
             for (--column; column >= 0; --column) {
                 for (int end = ends[column] - 1; end >= 0; --end) {
-                    System.out.print(matrix[column][end] + " ");
+                    System.out.print(toAbc(matrix[column][end]) + " ");
                 }
                 System.out.println();
             }
         } catch (IOException e) {
             System.out.println("Problem with IO" + e.getMessage());
         }
+    }
+
+    private static long parseAbc(String word) {
+        int pow = 1;
+        long res = 0;
+        char[] wordToCharArray = word.toCharArray();
+        int len = wordToCharArray.length;
+        for (int i = len - 1; i >= 0; i--) {
+            char token = wordToCharArray[i];
+            if (token == '-') {
+                continue;
+            }
+            res += (long) (wordToCharArray[i] - 'a') * pow;
+            pow *= 10;
+        }
+        if (word.charAt(0) == '-') {
+            res *= -1;
+        }
+        return res;
+    }
+
+    private static String toAbc(long number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        long originNumber = number;
+        do {
+            long digit = number % 10;
+            char letter = (char) ('a' + Math.abs(digit));
+            stringBuilder.append(letter);
+            number /= 10;
+        } while (number != 0);
+
+        if (originNumber < 0) {
+            stringBuilder.append('-');
+        }
+        return stringBuilder.reverse().toString();
     }
 }
